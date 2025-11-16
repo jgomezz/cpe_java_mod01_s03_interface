d3fa13a7bee6f5d0e39ece3569ed3d4483c06ea5
Developer <developer@Developers-MacBook-Pro.local>
2025-11-16 08:13:08 -0500

    update

--- src/pe/edu/tecsup/_08/exceptions/metodos/Aplicacion.java
+++ src/pe/edu/tecsup/_08/exceptions/metodos/Aplicacion.java
@@ -7,6 +7,8 @@ public class Aplicacion {
         int nro[] = {1, 2, 3, 4, 5, 6, 7, 12, 9, 13, 11, 12, 18, 14, 15};
 
         try {
+
+            /*
             for (int i = 0; i < nro.length; i++) {
                 if (nro[i] == 13) {
                     throw new Exception("Número anomalo " + nro[i]);
@@ -15,7 +17,12 @@ public class Aplicacion {
             }
 
             int valor = nro[20];
+            */
 
+            System.out.println("Inicio del proceso...");
+            int valor =  obtenerValor(nro, 15);
+            System.out.println("Valor obtenido: " + valor);
+            System.out.println("Fin del proceso...");
 
         } catch (ArrayIndexOutOfBoundsException e) {
             System.err.println("Error de indice fuera de rango: " + e.getMessage());
@@ -25,6 +32,10 @@ public class Aplicacion {
             System.out.println("Finalizo el bloque try-catch");
         }
 
-
     }
+
+    protected static int obtenerValor(int[] arreglo, int indice)  {
+        return arreglo[indice];
+    }
+
 }
