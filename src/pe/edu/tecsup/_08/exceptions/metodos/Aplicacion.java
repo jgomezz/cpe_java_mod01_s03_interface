cb08bd27b3f75e5ff7110470763d4cc487fbb21b
jgomezz <jgomezz@gmail.com>
2025-11-16 08:38:18 -0500

    update

--- src/pe/edu/tecsup/_08/exceptions/metodos/Aplicacion.java
+++ src/pe/edu/tecsup/_08/exceptions/metodos/Aplicacion.java
@@ -4,7 +4,7 @@ public class Aplicacion {
 
     public static void main(String[] args) {
 
-        int nro[] = {1, 2, 3, 4, 5, 6, 7, 12, 9, 13, 11, 12, 18, 14, 15};
+        int nro[] = {1123216, 2, 3, 4, 5, 6, 7, 12, 9, 13, 11, 12, 18, 14, 15};
 
         try {
 
@@ -26,24 +26,53 @@ public class Aplicacion {
             valor =  obtenerValor(nro, 5);
             System.out.println("Valor obtenido: " + valor);
 
-            valor =  obtenerValor(nro, 15);
-            System.out.println("Valor obtenido: " + valor);
+//            valor =  obtenerValor(nro, 15);
+//            System.out.println("Valor obtenido: " + valor);
 
+//            valor =  obtenerValor(nro, -1);
+//            System.out.println("Valor obtenido: " + valor);
+
+//            valor =  obtenerValor(null, 5);
+//            System.out.println("Valor obtenido: " + valor);
+
+            valor =  obtenerValor(nro, 0);
+            System.out.println("Valor obtenido: " + valor);
 
             System.out.println("Fin del proceso...");
 
         } catch (ArrayIndexOutOfBoundsException e) {
             System.err.println("Error de indice fuera de rango: " + e.getMessage());
         } catch (Exception e) {
-            System.err.println(e.getMessage());
+            System.err.println("Error general: " + e.getMessage());
         } finally {
             System.out.println("Finalizo el bloque try-catch");
         }
 
     }
 
-    protected static int obtenerValor(int[] arreglo, int indice)  {
+    /**
+     * Regla de negocio : Prohibir la lectura del primer dato
+     *
+     * @param arreglo
+     * @param indice
+     * @return
+     */
+    protected static int obtenerValor(int[] arreglo, int indice) throws Exception {
+
+        if (arreglo == null) {
+            throw new NullPointerException("El arreglo no puede ser nulo");
+        }
+
+        if (indice < 0 || indice >= arreglo.length) {
+            throw new ArrayIndexOutOfBoundsException("Indice fuera de rango: " + indice);
+        }
+
+        if (indice == 0) {
+            throw new Exception("No se permite leer el primer elemento");
+        }
+
         return arreglo[indice];
+
     }
 
 }
